8ad2c30cfeedf20ad22b39a616fe688f5ae299d8
Eslam Allam <eslamallam73@gmail.com>
2025-05-27 13:40:46 +0300

    feat(hypr): Move email client to special email workspace if it's already open

--- dot-config/hypr/UserScripts/EmailClient.sh
+++ dot-config/hypr/UserScripts/EmailClient.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 EMAIL_CLIENT="thunderbird"
+EMAIL_CLIENT_CLASS="thunderbird"
 
-if [ -z "$(pgrep -x $EMAIL_CLIENT)" ]; then
-  $EMAIL_CLIENT &
+EMAIL_OPEN="$(pgrep -x $EMAIL_CLIENT)"
+
+if [ -z "$(hyprctl workspaces -j | jq '.[] | select(.name=="special:email")')" ] && [ -n "$EMAIL_OPEN" ]; then
+	hyprctl dispatch movetoworkspace special:email,class:"$EMAIL_CLIENT_CLASS"
+elif [ -z "$EMAIL_OPEN" ]; then
+	$EMAIL_CLIENT &
 else
-  hyprctl dispatch togglespecialworkspace email
+	hyprctl dispatch togglespecialworkspace email
 fi
